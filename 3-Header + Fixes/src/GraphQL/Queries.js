8e315d03624e2605538860bb27206ba2d6b5e4a9
Higor Martinez <68279411+HigorMartinez@users.noreply.github.com>
2021-06-21 12:25:28 -0300

    Header + Fixes
    added code

---
+++ 3-Header + Fixes/src/GraphQL/Queries.js
@@ -0,0 +1,14 @@
+import { gql } from '@apollo/client';
+
+export const LOAD_DATA = gql`
+    query {
+        Country {
+            flag {svgFile}
+            name
+            capital
+            area
+            population
+            topLevelDomains {name}
+        }
+    }
+`;
