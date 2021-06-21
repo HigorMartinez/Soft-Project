8e88b82b56a333f185b5269832fdec4e5225d5c8
Higor Martinez <68279411+HigorMartinez@users.noreply.github.com>
2021-06-21 12:23:31 -0300

    Delete 3-Header + Fixes directory

--- 3-Header + Fixes/src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-
-import countriesSlice from './countries-slice';
-
-const store = configureStore({
-  reducer: { countries: countriesSlice.reducer },
-});
-
-export default store;
