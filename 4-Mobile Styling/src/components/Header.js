d10c3c828e9f9388c775a496a59e5fb25168c37c
Higor Martinez <68279411+HigorMartinez@users.noreply.github.com>
2021-06-24 16:36:50 -0300

    Mobile Styling
    added code

---
+++ 4-Mobile Styling/src/components/Header.js
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+
+import classes from './Header.module.css';
+
+const Header = (props) => {
+  
+  return (
+    <header className={classes.header}>
+      <Link to='/countries' >
+        <h1>Countries Panel</h1>
+      </Link>
+      {!props.notShow && <div className={classes.search}>
+        <input
+          id='search'
+          type='search'
+          placeholder={'Search country here...'}
+          value={props.enteredSearch}
+          onChange={props.searchChangeHandler}
+        />
+      </div>}
+    </header>
+  );
+};
+
+export default Header;
---
+++ 4-Mobile Styling/src/store/index.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import countriesSlice from './countries-slice';
+
+const store = configureStore({
+  reducer: { countries: countriesSlice.reducer },
+});
+
+export default store;
